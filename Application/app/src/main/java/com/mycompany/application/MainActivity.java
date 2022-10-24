645730ba38043bb5827baf6dabc74b7a509358f5
Thant Zin Win <tzw409@gmail.com>
2022-10-24 17:39:31 +0630

    add

---
+++ Application/app/src/main/java/com/mycompany/application/MainActivity.java
@@ -0,0 +1,18 @@
+package com.mycompany.application;
+ 
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity { 
+     
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+	
+} 
+/*don't forget to subscribe my YouTube channel for more Tutorial and mod*/
+/*
+https://youtube.com/channel/UC_lCMHEhEOFYgJL6fg1ZzQA */
